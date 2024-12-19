553d788b08f380352967d4283575a4540e9f85d2
xovest <olarumonica2016@gmail.com>
2024-12-19 23:44:23 +0200

    Moved internal class to separate file

--- ForwardCheckingOrar/ForwardCheckingOrar/Form1.cs
+++ ForwardCheckingOrar/ForwardCheckingOrar/Form1.cs
@@ -242,23 +242,6 @@ namespace ForwardCheckingOrar
             listSali.Items.Clear();
         }
 
-        class Sala
-        {
-            public string Nume { get; set; }
-            public List<(int Start, int End)> IntervaleRezervate { get; set; } = new List<(int Start, int End)>();
-
-            public override string ToString()
-            {
-                if (IntervaleRezervate.Count == 0)
-                {
-                    return $"{Nume} (Libera)";
-                }
-
-                var rezervari = string.Join(", ", IntervaleRezervate.Select(i => $"{i.Start}-{i.End}"));
-                return $"{Nume} (Rezervata: {rezervari})";
-            }
-        }
-
         private void btnRezervaSala_Click(object sender, EventArgs e)
         {
             if (listSali.SelectedItem is Sala salaSelectata &&
---
+++ ForwardCheckingOrar/ForwardCheckingOrar/Sala.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ForwardCheckingOrar
+{
+    class Sala
+    {
+        public string Nume { get; set; }
+        public List<(int Start, int End)> IntervaleRezervate { get; set; } = new List<(int Start, int End)>();
+
+        public override string ToString()
+        {
+            if (IntervaleRezervate.Count == 0)
+            {
+                return $"{Nume} (Libera)";
+            }
+
+            var rezervari = string.Join(", ", IntervaleRezervate.Select(i => $"{i.Start}-{i.End}"));
+            return $"{Nume} (Rezervata: {rezervari})";
+        }
+    }
+}
